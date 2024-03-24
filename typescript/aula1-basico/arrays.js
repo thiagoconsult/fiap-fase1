e481409f106c0e9a6464c20fb9a4e92efe106ac9
thiagoconsult <thiago.consultx@gmail.com>
2024-03-24 11:37:17 -0300

    Criação do arquivo `arrays.ts`

---
+++ typescript/aula1-basico/arrays.js
@@ -0,0 +1,15 @@
+// push() adiciona um ou mais elementos no final do array e retorna o tamanho
+// pop() remove e retorna o último elemento
+// unshift() adiciona um ou mais elementos no início array e retorna o tamanho
+// shift() remove e retorna o primeiro elemento do array
+var arr1 = [];
+console.log("Tamanho do array:", arr1.push(1));
+console.log("Tamanho do array:", arr1.push(2));
+console.log("Conteúdo do array:", arr1);
+console.log("Tamanho do array:", arr1.unshift(3));
+console.log("Tamanho do array:", arr1.unshift(4));
+console.log("Conteúdo do array:", arr1);
+console.log("Elemento removido do array:", arr1.pop());
+console.log("Conteúdo do array:", arr1);
+console.log("Elemento removido do array:", arr1.shift());
+console.log("Conteúdo do array:", arr1);
